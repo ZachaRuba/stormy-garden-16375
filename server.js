dc706dd5a4565fb13b84c07ec97097ffc005c421
Mr Ruba <zacharyjcanderson@gmail.com>
2018-01-09 14:07:34 -0800

    Made Heroku compatible by changing port from 3000 to process.env.PORT and by adding a start script to the package.json

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express = require('express');
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {res.send('Hello World!!')});
 
-app.listen(3000);
+app.listen(port);
