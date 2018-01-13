48852a29ee6c2945c6362fca74b2d2bf629d39a1
Mr Ruba <zacharyjcanderson@gmail.com>
2018-01-13 15:33:14 -0800

    Trying out this capture on mobile

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {res.send('Hey Bois')});
 
 app.listen(port);
